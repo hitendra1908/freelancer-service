8c7801541eeed0b280a4dd1426d1e484d4c7e761
Hitendra <hitendra1908@gmail.com>
2024-08-03 16:53:48 +0200

    api and web module created api module is to store all the interfaces of the api with its swagger documentation. web module will implement the api.

---
+++ freelancer-api/src/main/java/com/api/freelancer/user/User.java
@@ -0,0 +1,5 @@
+package com.api.freelancer.user;
+
+public record User(String userName, String firstName, String lastName, String email) {
+}
+
---
+++ freelancer-api/src/main/java/com/api/freelancer/user/UserApi.java
@@ -0,0 +1,42 @@
+package com.api.freelancer.user;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/api/freelancer")
+interface UserApi {
+
+    @GET
+    @Path("/users")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "View a list of available Users", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved users list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    List<User> getAllUsers() ;
+
+    @POST
+    @Path("/users")
+    @ApiOperation(value = "Add a User", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created a User"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    User addUser(User user);
+
+}
